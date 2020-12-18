be5eb1e3672dc3187a79a6c4acd026847b38c131
Asad Nisar <34133163+asadnisar98@users.noreply.github.com>
2020-12-18 05:10:53 +0500

    Update loader.js

--- src/components/loader.js
+++ src/components/loader.js
@@ -5,6 +5,30 @@ import anime from 'animejs';
 import styled from 'styled-components';
 import { IconLoader } from '@components/icons';
 
+const LoaderCustom = styled.div`
+
+		  font-size: 15rem;
+	    text-align: center;
+      height:90vh;
+      line-height: 90vh;
+	    color: #fcedd8;
+	    background: #d52e3f;
+	   	font-family: 'Niconne', cursive;
+	    font-weight: 700;
+      text-shadow: 5px 5px 0px #eb452b,
+                  10px 10px 0px #efa032,
+                  15px 15px 0px #46b59b,
+                  20px 20px 0px #017e7f,
+                  25px 25px 0px #052939,
+                  30px 30px 0px #c11a2b,
+                  35px 35px 0px #c11a2b,
+                  40px 40px 0px #c11a2b,
+                  45px 45px 0px #c11a2b;
+
+
+
+`;
+
 const StyledLoader = styled.div`
   ${({ theme }) => theme.mixins.flexCenter};
   position: fixed;
@@ -85,8 +109,8 @@ const Loader = ({ finishLoading }) => {
     <StyledLoader className="loader" isMounted={isMounted}>
       <Helmet bodyAttributes={{ class: `hidden` }} />
 
-      <div className="logo-wrapper">
-        <IconLoader />
+      <div>
+         <LoaderCustom>Asad</LoaderCustom>
       </div>
     </StyledLoader>
   );
